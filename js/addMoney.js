1243c0c03823c2977d125c9e811ff9c8b18f5ac6
mdtanvirislamrakib <mdtanvirislamrakib7@gmail.com>
2025-02-26 03:03:10 +0600

    pay bill section added

--- js/addMoney.js
+++ js/addMoney.js
@@ -4,6 +4,7 @@ document.getElementById('add-money-section').style.display = 'none';
 document.getElementById('cashout-section').style.display = 'none';
 document.getElementById('send-money-section').style.display = 'none';
 document.getElementById('bonus-section').style.display = 'none';
+document.getElementById('pay-bill-section').style.display = 'none';
 
 
 const addMoney = document.getElementById('add-money');
@@ -13,11 +14,13 @@ addMoney.addEventListener('click', function (event) {
     document.getElementById('latest-payment-section').style.display = 'none';
     document.getElementById('cashout-section').style.display = 'none';
     document.getElementById('send-money-section').style.display = 'none';
+    document.getElementById('pay-bill-section').style.display = 'none';
     document.getElementById('add-money').classList.add('active');
     document.getElementById('cashout').classList.remove('active');
     document.getElementById('send-money').classList.remove('active');
     document.getElementById('bonus-section').style.display = 'none';
     document.getElementById('get-bonus').classList.remove('active');
+    document.getElementById('pay-bill').classList.remove('active');
 })
 
 document.getElementById('cashout').addEventListener('click', function (event) {
@@ -26,11 +29,13 @@ document.getElementById('cashout').addEventListener('click', function (event) {
     document.getElementById('add-money-section').style.display = 'none';
     document.getElementById('send-money-section').style.display = 'none';
     document.getElementById('cashout-section').style.display = 'block';
+    document.getElementById('pay-bill-section').style.display = 'none';
     document.getElementById('add-money').classList.remove('active');
     document.getElementById('cashout').classList.add('active');
     document.getElementById('send-money').classList.remove('active');
     document.getElementById('bonus-section').style.display = 'none';
     document.getElementById('get-bonus').classList.remove('active');
+    document.getElementById('pay-bill').classList.remove('active');
 });
 
 
@@ -40,11 +45,13 @@ document.getElementById('send-money').addEventListener('click', function (event)
     document.getElementById('add-money-section').style.display = 'none';
     document.getElementById('cashout-section').style.display = 'none';
     document.getElementById('send-money-section').style.display = 'block';
+    document.getElementById('pay-bill-section').style.display = 'none';
     document.getElementById('add-money').classList.remove('active');
     document.getElementById('cashout').classList.remove('active');
     document.getElementById('send-money').classList.add('active');
     document.getElementById('bonus-section').style.display = 'none';
     document.getElementById('get-bonus').classList.remove('active');
+    document.getElementById('pay-bill').classList.remove('active');
 });
 
 
@@ -54,15 +61,31 @@ document.getElementById('get-bonus').addEventListener('click', function (event) 
     document.getElementById('add-money-section').style.display = 'none';
     document.getElementById('cashout-section').style.display = 'none';
     document.getElementById('send-money-section').style.display = 'none';
+    document.getElementById('pay-bill-section').style.display = 'none';
     document.getElementById('bonus-section').style.display = 'block';
     document.getElementById('add-money').classList.remove('active');
     document.getElementById('cashout').classList.remove('active');
     document.getElementById('send-money').classList.remove('active');
     document.getElementById('get-bonus').classList.add('active');
+    document.getElementById('pay-bill').classList.remove('active');
 });
 
 
 
+document.getElementById('pay-bill').addEventListener('click', function (event) {
+    event.preventDefault();
+    document.getElementById('latest-payment-section').style.display = 'none';
+    document.getElementById('add-money-section').style.display = 'none';
+    document.getElementById('cashout-section').style.display = 'none';
+    document.getElementById('send-money-section').style.display = 'none';
+    document.getElementById('bonus-section').style.display = 'none';
+    document.getElementById('pay-bill-section').style.display = 'block';
+    document.getElementById('add-money').classList.remove('active');
+    document.getElementById('cashout').classList.remove('active');
+    document.getElementById('send-money').classList.remove('active');
+    document.getElementById('get-bonus').classList.remove('active');
+    document.getElementById('pay-bill').classList.add('active');
+});
 
 
 
@@ -215,4 +238,41 @@ document.getElementById('bonus-btn').addEventListener('click', function (event) 
             ballance.innerText = totalAmmount;
         }
     }
+});
+
+
+
+
+// paybill section
+document.getElementById('pay-bill-btn').addEventListener('click', function (event) {
+    event.preventDefault();
+    // const accountNumber = document.getElementById('account-number').value;
+    const payBillNumber = document.getElementById('pay-bill-number').value;
+    // const pinNumber = document.getElementById('pin-number').value;
+    const payPinNumber = document.getElementById('pay-pin-number').value;
+    // const amount = document.getElementById('amount').value;
+    const PayAmount = document.getElementById('pay-amount').value;
+    const balance = document.getElementById('bdt');
+    const totalBallance = parseInt(balance.innerText) - parseFloat(PayAmount);
+
+    if (payBillNumber === '' || payPinNumber === '' || amount === '') {
+        alert('Please fill in all fields');
+    } else {
+        if (typeof parseInt(payBillNumber) !== 'number' || payBillNumber.length !== 11) {
+            alert('Invalid account number');
+        } else {
+            if (typeof parseInt(PayAmount) !== 'number') {
+                alert("Invalid Amount");
+            } else {
+                if (parseInt(payPinNumber) !== 1234) {
+                    alert('Invalid pin number');
+                } else {
+                    balance.innerText = totalBallance;
+                    alert("Pay Bill successfully");
+                }
+            }
+
+        }
+    }
+
 });
